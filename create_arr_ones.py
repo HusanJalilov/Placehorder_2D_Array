9476ef5deddd096d452a7c1667f279bef8423c36
coder_uz <husanjalilov00777@gmil.com>
2022-03-23 14:21:24 +0500

    arr ones

--- create_arr_ones.py
+++ create_arr_ones.py
@@ -7,3 +7,7 @@ def create_arr_ones(n,m):
     Returns:
         list: 2D list
     """
+    a=0
+    Matrix = [[1 for x in range(m)] for y in range(n)] 
+    return Matrix
+print(create_arr_ones(4,3))
